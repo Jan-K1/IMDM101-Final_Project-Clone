c3964708edcf3fc67a61131e2df71b13e90a7cca
ThraceSherman <thraciebear@gmail.com>
2025-12-09 10:20:21 -0500

    Game over scene and moved player

---
+++ Assets/GameOverScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Dungeon_Demo");
+    }
+}
